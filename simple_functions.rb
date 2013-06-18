58cc1c544ff3a8b211fe726de4335698f635faf2
Stephen McGillen <smcgillen@hotmail.com>
2013-06-18 16:27:44 +1000

    Simple functions added

--- simple_functions.rb
+++ simple_functions.rb
@@ -1,10 +1,31 @@
-def Add(a,b)
-	a + b
-	
+def get_inputs()
+	puts "Enter first number"
+	first = gets.chomp.to_i
+	puts "Enter second number"
+	second = gets.chomp.to_i
+	return first,second
 end
 
-def Subtract(a,b)
-	a - b
-
+def do_add()
+	a,b = get_inputs()
+	result = a + b
+    puts "Result of #{a} + #{b} = #{result}"
 end
-	
+
+def do_subtract()
+	a,b = get_inputs()
+	result = a - b
+    puts "Result of #{a} - #{b} = #{result}"
+end
+
+def do_multiply()
+	a,b = get_inputs()
+	result = a * b
+    puts "Result of #{a} * #{b} = #{result}"
+end
+
+def do_divide()
+	a,b = get_inputs()
+	result = a / b
+    puts "Result of #{a} / #{b} = #{result}"
+end
