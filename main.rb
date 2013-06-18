245b0a19786869a47445d3ed259acac1075525a4
RRails <sanjita_quinn@hotmail.com>
2013-06-18 17:35:15 +1000

    Added basic and advanced functions

--- main.rb
+++ main.rb
@@ -6,30 +6,12 @@
 require_relative 'simple_functions.rb'
 require_relative 'advanced_functions.rb'
 
-def menu() #Menu Definition
-puts "Choose calculator"
-puts "Enter S for simple calculations, Enter A for advanced calculations"
-puts "Enter Q for quit"
-calculator = gets.chomp.upcase #User can enter upper or lower case
-end
-
-calculator = menu()
-
-while calculator != 'Q'
-	if calculator == 'S'
-		puts "Enter A for add"
-		puts "Enter S for Subtract"
-		puts "Enter D for Divide"
-		puts "Enter M for Multiply"
-	elsif calculator == 'A'
-		puts "Enter R for square root"
-		puts "Enter E for Exponentiation"
-	elsif calculator != ( "A" || "S")
-		puts "Not valid option"
-	end
-
+def basic_calc
+	puts "Enter A for add"
+	puts "Enter S for Subtract"
+	puts "Enter D for Divide"
+	puts "Enter M for Multiply"
 	response = gets.chomp.upcase
-
 	if response == "A"
 		do_add()
 	elsif response == "S"
@@ -38,10 +20,36 @@ while calculator != 'Q'
 		do_divide()
 	elsif response == "M"
 		do_multiply()
-	elsif response == "R" 
+	end
+end
+
+def advanced_calc
+	puts "Enter R for square root"
+	puts "Enter E for Exponentiation"
+	response = gets.chomp.upcase
+	if response == "R" 
 		do_square_root()
 	elsif response == "E"
 		do_exponential()
+	end
+end
+
+def menu() #Menu Definition
+	puts "Choose calculator"
+	puts "Enter S for simple calculations, Enter A for advanced calculations"
+	puts "Enter Q for quit"
+	calculator = gets.chomp.upcase #User can enter upper or lower case
+end
+
+calculator = menu()
+
+while calculator != 'Q'
+	if calculator == 'S'
+		basic_calc
+	elsif calculator == 'A'
+		advanced_calc
+	else
+		puts "Not valid option"
 	end
 
 	puts " "
